8dcdc861374f6c7aa119e467e8e3e9f798264838
brando <192037903+brando008@users.noreply.github.com>
2025-07-15 10:54:44 -0500

    Create fictitious points from the emotion means

--- song_searching.py
+++ song_searching.py
@@ -12,6 +12,7 @@ assert df_song_info.index.equals(df_features.index), "Index mismatch!"
 
 song_name = "Put You On"
 matching = df_song_info[df_song_info["Song"] == song_name].index
+artist_name = df_song_info.loc[matching[0],"Artist(s)"]
 
 if matching.empty:
     print("Song not found")
@@ -19,4 +20,4 @@ else:
     song_index = matching[0]
     song_vector = [df_features.loc[song_index].values]
 
-    print(f" Song: {song_name} | Vector: {song_vector}")
+    print(f" Song: {song_name} | Artist: {artist_name}| Vector: {song_vector}")
