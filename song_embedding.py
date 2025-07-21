85264a287f8cffbf0bad6bc62bfa5f9502d34218
brando008 <brando008users.noreply.github.com>
2025-07-21 11:17:49 -0500

    Created a npy search for all the songs/artists in the database via embedding

---
+++ song_embedding.py
@@ -0,0 +1,19 @@
+from sentence_transformers import SentenceTransformer
+import pandas as pd
+import numpy as np
+from data_utils import load_data
+from tqdm import tqdm
+
+df = load_data('data/song_data.csv', index=True)
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def embed_song_artist(row):
+    song_artist_str = f"{row['Song']} by {row['Artist(s)']}"
+    return model.encode(song_artist_str)
+
+tqdm.pandas()
+
+df["embedding"] = df.progress_apply(embed_song_artist, axis=1)
+
+embeddings_array = np.vstack(df["embedding"].values)
+np.save("data/song_embeddings.npy", embeddings_array)
