4e1b2b95b0fc8d18939d64f7b36762777dfa1eb3
brando008 <brando008users.noreply.github.com>
2025-08-01 14:18:07 -0500

    got rid of the aws

--- s3_utils.py
+++ s3_utils.py
@@ -8,15 +8,15 @@ import streamlit as st
 def get_s3_client():
     return boto3.client(
         "s3",
-        aws_access_key_id = st.secrets["aws"]["AWS_ACCESS_KEY_ID"],
-        aws_secret_access_key = st.secrets["aws"]["AWS_SECRET_ACCESS_KEY"],
-        region_name = st.secrets["aws"]["AWS_REGION"],
+        aws_access_key_id = st.secrets["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key = st.secrets["AWS_SECRET_ACCESS_KEY"],
+        region_name = st.secrets["AWS_REGION"],
     )
 
 def load_csv_from_s3(file_name, index_col=None):
     s3 = get_s3_client()
-    bucket = st.secrets["aws"]["S3_BUCKET_NAME"]
-    folder = st.secrets["aws"]["S3_FOLDER"]
+    bucket = st.secrets["S3_BUCKET_NAME"]
+    folder = st.secrets["S3_FOLDER"]
     key = f"{folder}{file_name}"
 
     response = s3.get_object(Bucket=bucket, Key=key)
@@ -25,8 +25,8 @@ def load_csv_from_s3(file_name, index_col=None):
 
 def load_json_from_s3(file_name):
     s3 = get_s3_client()
-    bucket = st.secrets["aws"]["S3_BUCKET_NAME"]
-    folder = st.secrets["aws"]["S3_FOLDER"]
+    bucket = st.secrets["S3_BUCKET_NAME"]
+    folder = st.secrets["S3_FOLDER"]
     key = f"{folder}{file_name}"
 
     response = s3.get_object(Bucket=bucket, Key=key)
@@ -35,8 +35,8 @@ def load_json_from_s3(file_name):
 
 def load_binary_from_s3(file_name):
     s3 = get_s3_client()
-    bucket = st.secrets["aws"]["S3_BUCKET_NAME"]
-    folder = st.secrets["aws"]["S3_FOLDER"]
+    bucket = st.secrets["S3_BUCKET_NAME"]
+    folder = st.secrets["S3_FOLDER"]
     key = f"{folder}{file_name}"
 
     response = s3.get_object(Bucket=bucket, Key=key)
