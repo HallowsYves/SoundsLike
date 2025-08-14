29cd4d68d55f2232c3a579030b82a3727ee02e97
HallowsYves <vyves246@gmail.com>
2025-08-14 14:39:16 -0700

    Removed LFS

--- main.py
+++
@@ -1,5 +0,0 @@
-from sounds_like_utils import find_similar_songs
-
-if __name__ == "__main__":
-    prompt = input("Enter a song prompt (e.g., 'give me songs like Moon by Kanye West'): ")
-    find_similar_songs(prompt)
